9efd7faed23914d9c566044476d91012e7307881
Stephen Buchanan <stephen.buchanan@reaktor.com>
2025-09-22 13:26:51 +0200

    incorporating markdown rendering

--- src/components/RenderedSticky.tsx
+++ src/components/RenderedSticky.tsx
@@ -1,11 +1,13 @@
+import Markdown from 'react-markdown';
 import styles from './RenderedSticky.module.css';
 
 export default function RenderedSticky({ id, title, body, position }: Sticky) {
-  
   return (
-    <div className={styles.sticky} >
+    <div className={styles.sticky}>
       <h1>{title}</h1>
-      <div className={styles.body}>{body}</div>
+      <div className={styles.body}>
+        <Markdown>{body}</Markdown>
+      </div>
     </div>
   );
 }
