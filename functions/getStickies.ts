7c6032907c87249eebb7708a3ff9610c1d84b7bd
Stephen Buchanan <stephen.buchanan@reaktor.com>
2025-09-22 14:57:33 +0200

    remove some logging

--- functions/getStickies.ts
+++ functions/getStickies.ts
@@ -3,7 +3,7 @@ import { getStore } from '@netlify/blobs';
 export default async () => {
   const store = getStore('stickies-store');
   const list = await store.list();
-  console.log('fetched store list:', list);
+  // console.log('fetched store list:', list);
   const { blobs } = list;
 
   const stickies = await Promise.all(
